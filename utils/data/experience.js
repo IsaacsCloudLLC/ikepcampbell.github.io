de0dece847015e4589662dcc5e7f37fabe7a2382
Ike <isaacpcampbell98@gmail.com>
2024-03-23 20:07:01 -0500

    update

---
+++ utils/data/experience.js
@@ -0,0 +1,57 @@
+export const experiences = [
+  {
+    id: 1,
+    title: "Junior Devops Engineer",
+    company: "YASH Technologies",
+    duration: "(Jun 2019 - Oct 2020)",
+    location: "Moline, Illinois",
+    url: "https://www.yash.com/",
+    logo: "yash.jpg"
+  },
+  {
+    id: 2,
+    title: "Site Reliability Engineer",
+    company: "Amount",
+    duration: "(Oct 2022 - Jun 2022)",
+    location: "Remote",
+    url: "https://www.amount.com/",
+    logo: "amount.jpg"
+  },
+  {
+    id: 3,
+    title: "Platform Engineer",
+    company: "Bloom Credit",
+    duration: "(Jul 2022 - March 2023)",
+    location: "Remote",
+    url: "https://bloomcredit.io/",
+    logo: "bloomcredit.jpg"
+  },
+  {
+    id: 4,
+    title: "Devops Consultant",
+    company: "Adiuvat Consulting",
+    duration: "(Oct 2022 - March 2023)",
+    location: "Remote",
+    url: "https://adiuvat.io/",
+    logo: "adiuvat.jpg"
+  },
+  {
+    id: 5,
+    title: "Site Reliability Engineer",
+    company: "Haven Technologies",
+    duration: "(March 2023 - Aug 2023)",
+    location: "Remote",
+    url: "https://www.haventech.us/",
+    logo: "haven.jpg"
+  },
+
+  {
+    id: 6,
+    title: 'Staff SRE',
+    company: "TechJoy/Joy of Coding",
+    duration: "(Sep 2023 - Present)",
+    location: "Remote",
+    url: "https://joyofcoding.academy/",
+    logo: "techjoy.jpg"
+  }
+]
