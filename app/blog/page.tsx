c2e887a2fc8ab3d2c434120c5a380a65b19d4f9e
Ike <isaacpcampbell98@gmail.com>
2024-04-09 15:02:00 -0500

    THIS should work

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
   return (
     <section>
       <h1 className="font-semibold text-3xl mb-8 tracking-tighter">Blog</h1>
-      {/* <BlogPosts /> */}
+      <BlogPosts />
       <p className='font-semibold text-2xl mb-8 tracking-tighter'> Coming Soon! </p>
     </section>
   )
--- utils/data/contactsData.js
+++
@@ -1,5 +0,0 @@
-export const contactsData = {
-    email: 'support@isaacs.cloud',
-    github: 'https://github.com/IkePCampbell',
-    linkedIn: 'https://www.linkedin.com/in/isaac-campbell/',
-}
